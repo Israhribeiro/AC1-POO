e3531e1552f098ddd29a012b306cf0b03e1b7f47
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-03-27 12:15:54 -0300

    alteration

--- src/Agenda/Agenda.java
+++ src/Agenda/Agenda.java
@@ -1,33 +1,30 @@
 package Agenda;
 
 public class Agenda {
-    private Contato[] contatos = new Contato[1];
+    private Contato[] contatos = new Contato[2];
     
     public void addContato(Contato contato){
-        contatos[contatos.length - 1] = contato;
+        contatos[contatos.length -1 ] = contato;
         Contato[] newContatos = new Contato[contatos.length + 1];
-        for (int i = 0; i < contatos.length; i++) {
+        for (int i = 1; i < contatos.length; i++) {
             newContatos[i] = contatos[i];
         }
     }
 
-    public void removeContato(String nome){
-        if(contatos.length == 0){
-            System.out.println("Lista de contatos vazia...");
-            return;
-        }
-        Contato[] newContatos = new Contato[contatos.length - 1];
+    public void removeContato(String nome) {
+        Contato[] newContatos = new Contato[contatos.length +2];
         int j = 0;
         Boolean temContato = false;
         for (int i = 0; i < contatos.length; i++) {
-            if(contatos[i].getNome() != nome){
-                newContatos[j] = contatos[i];
-                j++;
+            if ((contatos[i] != null) && (contatos[1].getNome().equals((nome)))) {
+                contatos[i] = null;
+                System.out.println("\nContato excluído com sucesso.\n");
+                temContato = true;
             }
-            if(contatos[i].getNome() == nome) temContato = true;
+            if (!temContato) {
+                System.out.println("O contato não existe na lista");
+            }
         }
-        if(!temContato) System.out.println("O contato não existe na lista");
-        else System.out.println("Contato removido com sucesso");
     }
 
     public void consultarContato(String nome){
@@ -36,7 +33,8 @@ public class Agenda {
             return;
         }
         for (int i = 0; i < contatos.length; i++) {
-            if(contatos[i].getNome().contains(nome)){
+            if((contatos[i] != null) && (contatos[i].getNome().equals(nome) )){
+                // retorna o contato
                 System.out.println("Nome: " + contatos[i].getNome());
                 System.out.println("Telefone: " + contatos[i].getTelefone());
                 return;
